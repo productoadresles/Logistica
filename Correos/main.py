2b820cb958b6f6517f60b7b7f12903c21d4ab9fc
Zeimoto <51506627+Zeimoto@users.noreply.github.com>
2024-03-15 01:54:46 +0000

    preregistro ready for integration

--- Correos/main.py
+++ Correos/main.py
@@ -12,11 +12,7 @@ import json
 import xml.dom.minidom
 import xmltodict
 
-# from correos_preregistro.vars import PRE_URL, URL
-# from correos_preregistro.client import RawClient as Client
-# from correos_preregistro.requests.preregistro_envio import  as Client
-# from correos_preregistro.resources import Package, Receiver, Sender
-from vars import PRE_URL, URL
+from vars import PICKUP_AUTH, PICKUP_PRE_AUTH, PICKUP_PRE_URL, PICKUP_URL, PR_PRE_URL, PR_URL, PREREGISTER_AUTH, PREREGISTER_PRE_AUTH
 
 import xml.etree.ElementTree as ET
 from datetime import datetime
@@ -29,6 +25,27 @@ class back(BaseModel):
     id_usuario: str
 
 
+
+## Pre registro response package list
+class PreRegister_Response_File(BaseModel):
+    nombreFicheiro: str
+    tipoDoc: str
+    ficheiroBinario: str
+    
+## Pre registro response package list
+class PreRegister_Response_Package(BaseModel):
+    numBulto: int
+    codEnvio: str
+    codManifiesto: str
+    ficheiro: PreRegister_Response_File
+
+## Pre registro response
+class PreRegister_Response(BaseModel):
+    resultado: int
+    codExpedicion: str
+    fechaRespuesta: str
+    bultos: List[PreRegister_Response_Package]
+    
 class Package(BaseModel):
     peso: int
     largo: Optional[int] = None
@@ -40,10 +57,7 @@ class Shipment(BaseModel):
     prod: int
     id_agencia: str
     cantidad: int
-    webservice: str
     client_uid: str
-    pickup_from: Optional[str] = None
-    pickup_until: Optional[str] = None
     codigos_origen: str
     codigos_destino: str
     direccion_salida: str
@@ -54,13 +68,10 @@ class Shipment(BaseModel):
     poblacion_salida: str
     telefono_llegada: Optional[str] = None
     telefono_salida: str
-    fecha_recogida: str
-    fecha_entrega: Optional[str] = None
+    fecha_operacion: str
     nombre_llegada: str
     nombre_salida: str
     packageList: List[Package]
-    user: str
-    password: str
 
 class ReqStatus(BaseModel):
     prod: int
@@ -77,13 +88,15 @@ async def root():
     return {"message": "Hello World"}
     
 
+@app.post("/PreRegister")
+async def preregister_correos(req: Shipment):
 
-@app.post("/shipment_create_correos")
-async def shipment_create_correos(req: Shipment):
+    #auth = (user, password)
+    auth = (PREREGISTER_PRE_AUTH if req.prod == 0 else PREREGISTER_AUTH)
 
     # URL CORREOS
-    url = (PRE_URL if req.prod == 0 else URL)#+"?wsdl"
-    print('URL: ',url)
+    url = (PR_PRE_URL if req.prod == 0 else PR_URL)
+    #print('URL: ',url)
     
     #  Generación de la referencia tipo C
     def random_with_N_digits(n):
@@ -97,85 +110,82 @@ async def shipment_create_correos(req: Shipment):
         <soapenv:Body>"""
 
     payload2 = f""""""
-    if (req.webservice == 'shipment'):
-        #setup the first part of the shipment info (addresses and contacts)
-        payloadB = f"""
-            <prer:PreregistroEnvioMultibulto>
-                <prer:FechaOperacion>{req.fecha_recogida}</prer:FechaOperacion>
-                <prer:CodEtiquetador>XXX1</prer:CodEtiquetador>
-                <prer:Care>000000</prer:Care>
-                <prer:TotalBultos>{req.cantidad}</prer:TotalBultos>
-                <prer:ModDevEtiqueta>2</prer:ModDevEtiqueta>
-                <prer:Remitente>
+    
+    #setup the first part of the shipment info (addresses and contacts)
+    payloadB = f"""
+        <prer:PreregistroEnvioMultibulto>
+            <prer:FechaOperacion>{req.fecha_operacion}</prer:FechaOperacion>
+            <prer:CodEtiquetador>XXX1</prer:CodEtiquetador>
+            <prer:Care>000000</prer:Care>
+            <prer:TotalBultos>{req.cantidad}</prer:TotalBultos>
+            <prer:ModDevEtiqueta>2</prer:ModDevEtiqueta>
+            <prer:Remitente>
+            
+                <prer:Identificacion>
+                <prer:Nombre>{req.nombre_salida}</prer:Nombre>
+                </prer:Identificacion>
                 
-                    <prer:Identificacion>
-                    <prer:Nombre>{req.nombre_salida}</prer:Nombre>
-                    </prer:Identificacion>
-                    
-                    <prer:DatosDireccion>
-                    <prer:Direccion>{req.direccion_salida}</prer:Direccion>
-                    <prer:Localidad>{req.poblacion_salida}</prer:Localidad>
-                    </prer:DatosDireccion>
-                    
-                    <prer:CP>{req.codigos_origen}</prer:CP>
-                    <prer:Telefonocontacto>{req.telefono_salida}</prer:Telefonocontacto>
-                    <prer:Email>{req.email_salida}</prer:Email>
+                <prer:DatosDireccion>
+                <prer:Direccion>{req.direccion_salida}</prer:Direccion>
+                <prer:Localidad>{req.poblacion_salida}</prer:Localidad>
+                </prer:DatosDireccion>
                 
-                    <prer:DatosSMS>
-                    <prer:NumeroSMS>{req.telefono_salida}</prer:NumeroSMS>
-                    </prer:DatosSMS>
-                    
-                </prer:Remitente>
+                <prer:CP>{req.codigos_origen}</prer:CP>
+                <prer:Telefonocontacto>{req.telefono_salida}</prer:Telefonocontacto>
+                <prer:Email>{req.email_salida}</prer:Email>
+            
+                <prer:DatosSMS>
+                <prer:NumeroSMS>{req.telefono_salida}</prer:NumeroSMS>
+                </prer:DatosSMS>
                 
-                <prer:Destinatario>
-                    <prer:Identificacion>
-                    <prer:Nombre>{req.nombre_llegada}</prer:Nombre>
-                    </prer:Identificacion>
-                    
-                    <prer:DatosDireccion>
-                    <prer:Direccion>{req.direccion_llegada}</prer:Direccion>
-                    <prer:Localidad>{req.poblacion_llegada}</prer:Localidad>
-                    <prer:Provincia>{req.poblacion_llegada}</prer:Provincia>
-                    </prer:DatosDireccion>
-                    
-                    <prer:CP>{req.codigos_destino}</prer:CP>
-                    <prer:Telefonocontacto>{req.telefono_llegada}</prer:Telefonocontacto>
-                    <prer:Email>{req.email_llegada}</prer:Email>
-                    <DatosSMS>
-                    <NumeroSMS>{req.telefono_llegada}</NumeroSMS>
-                    </DatosSMS>
-                </prer:Destinatario>
-                <prer:Envios>"""        
-        print(req.cantidad)
-        for i in range(req.cantidad):
-            print('Current doc', i)
-            packages = req.packageList[i]
-            #print(packages)
-            payloadPackages = f"""
-                        <prer:Envio>
-                            <prer:NumBulto>{i+1}</prer:NumBulto>
-                            <prer:Pesos>
-                                <prer:Peso>
-                                    <prer:TipoPeso>R</prer:TipoPeso>
-                                    <prer:Valor>{packages.peso}</prer:Valor>
-                                </prer:Peso>
-                            </prer:Pesos>
-                            <prer:Largo>{packages.largo}</prer:Largo>
-                            <prer:Alto>{packages.alto}</prer:Alto>
-                            <prer:Ancho>{packages.ancho}</prer:Ancho>
-                            <Observaciones1>{packages.observaciones_salida}</Observaciones1>
-                        </prer:Envio>                        
-                    """
-            payloadB += payloadPackages
+            </prer:Remitente>
+            
+            <prer:Destinatario>
+                <prer:Identificacion>
+                <prer:Nombre>{req.nombre_llegada}</prer:Nombre>
+                </prer:Identificacion>
+                
+                <prer:DatosDireccion>
+                <prer:Direccion>{req.direccion_llegada}</prer:Direccion>
+                <prer:Localidad>{req.poblacion_llegada}</prer:Localidad>
+                <prer:Provincia>{req.poblacion_llegada}</prer:Provincia>
+                </prer:DatosDireccion>
+                
+                <prer:CP>{req.codigos_destino}</prer:CP>
+                <prer:Telefonocontacto>{req.telefono_llegada}</prer:Telefonocontacto>
+                <prer:Email>{req.email_llegada}</prer:Email>
+                <DatosSMS>
+                <NumeroSMS>{req.telefono_llegada}</NumeroSMS>
+                </DatosSMS>
+            </prer:Destinatario>
+            <prer:Envios>"""  
+    
+    #build all packages inside Envios
+    for i in range(req.cantidad):
+        packages = req.packageList[i]
+        payloadPackages = f"""
+                    <prer:Envio>
+                        <prer:NumBulto>{i+1}</prer:NumBulto>
+                        <prer:Pesos>
+                            <prer:Peso>
+                                <prer:TipoPeso>R</prer:TipoPeso>
+                                <prer:Valor>{packages.peso}</prer:Valor>
+                            </prer:Peso>
+                        </prer:Pesos>
+                        <prer:Largo>{packages.largo}</prer:Largo>
+                        <prer:Alto>{packages.alto}</prer:Alto>
+                        <prer:Ancho>{packages.ancho}</prer:Ancho>
+                        <Observaciones1>{packages.observaciones_salida}</Observaciones1>
+                    </prer:Envio>                        
+                """
+        payloadB += payloadPackages
 
-        payload2 += payloadB + f"""
-                </prer:Envios>  
-                <prer:EntregaParcial></prer:EntregaParcial>
-                    <prer:CodProducto>S0132</prer:CodProducto>
-                    <prer:ReferenciaExpedicion></prer:ReferenciaExpedicion>
-                    <prer:ModalidadEntrega>ST</prer:ModalidadEntrega>
-                    <prer:TipoFranqueo>FP</prer:TipoFranqueo>
-                </prer:PreregistroEnvioMultibulto>   """    
+    payload2 += payloadB + f"""
+            </prer:Envios>  
+            <prer:CodProducto>S0132</prer:CodProducto>
+            <prer:ModalidadEntrega>ST</prer:ModalidadEntrega>
+            <prer:TipoFranqueo>FP</prer:TipoFranqueo>
+            </prer:PreregistroEnvioMultibulto>   """    
     payload3 = f"""
         </soapenv:Body>
     </soapenv:Envelope>"""
@@ -186,32 +196,56 @@ async def shipment_create_correos(req: Shipment):
     }
 
     # Request
-    print(payload+payload2+payload3)
-    response = requests.post( url = url, headers=headers, auth=(req.user, req.password), data=(payload+payload2+payload3).encode("utf-8"))
+    #print(payload+payload2+payload3)
+    response = requests.post( url = url, headers=headers, auth=auth, data=(payload+payload2+payload3).encode("utf-8"))
     contesta = (response.text)
-    print('Response: ',response)
-    print('Contesta: ',contesta)
+    #print('Response: ',response)
+    #print('Contesta: ',contesta)
 
-    # Parsing
     contesta_dict = xmltodict.parse(contesta)
 
-    # print(contesta_dict) 
+    bulto = contesta_dict['soapenv:Envelope']['soapenv:Body']['RespuestaPreregistroEnvioMultibulto']['Bultos']['Bulto']
 
-
-    # Resultados
-    if(req.webservice == 'shipment'):
-        resultado_dict = contesta_dict['soap:Envelope']['soap:Body']['GrabaServiciosResponse']['GrabaServiciosResult']['Servicios']['Envio']['Resultado']['@return']
-        if resultado_dict == '0':
-            codigo_envio = contesta_dict['soap:Envelope']['soap:Body']['GrabaServiciosResponse']['GrabaServiciosResult']['Servicios']['Envio']['@codexp'] 
-            etiqueta = contesta_dict['soap:Envelope']['soap:Body']['GrabaServiciosResponse']['GrabaServiciosResult']['Servicios']['Envio']['Etiquetas']['Etiqueta']['#text']
-            if req.id_agencia == "":
-                agencia = "329"
-            else:
-                agencia = req.id_agencia
-            devuelve = {'resultado': '1', "codigo_envio": codigo_envio, "codigo_recogida": codigo_envio, "id_agencia": agencia, "etiqueta": etiqueta}
-        else:
-            devuelve = {'resultado': "0", "codigo_envio": resultado_dict, "codigo_recogida": contesta_dict['soap:Envelope']['soap:Body']['GrabaServiciosResponse']['GrabaServiciosResult']['Servicios']['Envio']['Errores']['Error']}
-
-
+    devuelve = PreRegister_Response(
+        resultado = contesta_dict['soapenv:Envelope']['soapenv:Body']['RespuestaPreregistroEnvioMultibulto']['Resultado'],
+        codExpedicion = contesta_dict['soapenv:Envelope']['soapenv:Body']['RespuestaPreregistroEnvioMultibulto']['CodExpedicion'],
+        fechaRespuesta = contesta_dict['soapenv:Envelope']['soapenv:Body']['RespuestaPreregistroEnvioMultibulto']['FechaRespuesta'],
+        bultos = [PreRegister_Response_Package(
+            numBulto = b['NumBulto'], 
+            codEnvio = b['CodEnvio'], 
+            codManifiesto = b['CodManifiesto'], 
+            ficheiro = PreRegister_Response_File(nombreFicheiro=b['Etiqueta']['Etiqueta_pdf']['NombreF'],
+                                               tipoDoc=b['Etiqueta']['Etiqueta_pdf']['Tipo_Doc'],
+                                               ficheiroBinario=b['Etiqueta']['Etiqueta_pdf']['Fichero']
+        )) for b in bulto]
+    )
     # print(payload+payload2+payload3)
+    #print('Resultado:',devuelve.resultado, ', codExpedicion:', devuelve.codExpedicion, ', fechaRespuesta:',devuelve.fechaRespuesta, ', numBultos:',len(devuelve.bultos))
     return devuelve
+
+@app.post("/Pickup")
+async def pickup_correos (req: Shipment):
+
+    if req.prod == 0:
+        auth = PICKUP_PRE_AUTH
+        url = PICKUP_PRE_URL
+        contract = "99999999"
+        detallable = "99999999"
+        Annex = "091"
+    else:
+        auth = PICKUP_AUTH
+        url = PICKUP_URL
+
+    # Cabeceras
+    headers = {
+        'Content-Type': 'text/xml; charset=UTF-8'
+    }
+    payload = f""""""
+
+    #response = requests.post(url=url, auth=auth, headers=headers data=payload)
+
+
+@app.post("/ReqStatus")
+async def reqstatus_correos (req: Shipment):
+    
+    url = ""
--- Correos/vars.py
+++ Correos/vars.py
@@ -1,2 +1,12 @@
-PRE_URL = "https://preregistroenviospre.correos.es/?wsdl"
-URL = "https://preregistroenvios.correos.es/preregistroenvios"
+#URLs for PREREGISTRO
+PR_PRE_URL = "https://preregistroenviospre.correos.es/?wsdl"
+PR_URL = "https://preregistroenvios.correos.es/?wsdl"
+#URLs for Recogidas
+PICKUP_PRE_URL = ""
+PICKUP_URL = ""
+#Authentication for Recogidas
+PICKUP_PRE_AUTH = ("PRUEBAWSRECOGIDAS", )
+PICKUP_AUTH = ()
+#Authentication for PreRegistro
+PREREGISTER_PRE_AUTH = ("wusutest", "mAHB9yi0")
+PREREGISTER_AUTH = ("wusutest", "mAHB9yi0")
